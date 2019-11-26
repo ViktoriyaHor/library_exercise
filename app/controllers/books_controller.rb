7c13fc268a2c9ddd500de6093e1aeca62a40de5f
ViktoriyaHor <vhoroshun.face@gmail.com>
2019-11-26 13:58:32 +0200

    add ajax in history change

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -75,24 +75,23 @@ class BooksController < ApplicationController
   def take
     if current_user
       @book.update_attribute(:status, true)
-      @book.histories.create(user_id: current_user.id)
+      @history = @book.histories.create(user_id: current_user.id)
       respond_to do |format|
         format.html { redirect_to @book, notice: 'Book was successfully took.' }
-        format.json { head :no_content }
+        format.json { render json: @history }
         format.js { flash.now[:notice] = 'Book was successfully took.' }
       end
     else
       respond_to do |format|
         format.html { redirect_to @book, notice: 'You need to sign in or sign up before continuing.' }
         format.json { head :no_content }
-        format.js { flash.now[:notice] = 'You need to sign in or sign up before continuing.' }
+        format.js { flash.now[:notice] = 'You need to sign in or sign up before continuing.'}
       end
     end
 
   end
 
   def return
-
     @history = @book.histories.find_by_user(current_user.id).where(return_at: nil).first
     @history.update_attributes(return_at: Time.now)
     @book.update_attribute(:status, false)
