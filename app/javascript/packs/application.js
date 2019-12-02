f6e4193d2d4ce2ba2c5937609d3b02757bb81043
ViktoriyaHor <vhoroshun.face@gmail.com>
2019-12-02 13:41:03 +0200

    config for heroku

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,10 @@ require("@rails/activestorage").start()
 require("turbolinks").start()
 require("channels")
 require("jquery")
-// window.jQuery = $;
-// window.$ = $;
+
+window.jQuery = $;
+window.$ = $;
+
 import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -114,4 +114,9 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.assets.js_compressor = :uglifier
+  #config.assets.js_compressor = Uglifier.new(harmony: true)
+  config.require_master_key = true
+  # Do not dump schema after migrations.
+  #config.active_record.dump_schema_after_migration = false
 end
