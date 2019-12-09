49ab1836f6e60f31e0364a00e45571e03d4119b4
ViktoriyaHor <vhoroshun.face@gmail.com>
2019-12-09 13:56:23 +0200

    commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,17 +57,21 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "library_exercise_production"
 
-  config.action_mailer.default_url_options = { host: ENV['HOST'] }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      address: 'smtp.gmail.com',
-      port: 587,
-      domain: 'gmail.com',
-      user_name: ENV['USERNAME'],
-      password: ENV['PASSWORD'],
-      authentication: :plain,
-      enable_starttls_auto: true
-  }
+
+
+  #config.action_mailer.default_url_options = { host: ENV['HOST'] }
+  #config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.smtp_settings = {
+  #    address: 'smtp.gmail.com',
+  #    port: 587,
+  #    domain: 'gmail.com',
+  #    user_name: ENV['USERNAME'],
+  #    password: ENV['PASSWORD'],
+  #    authentication: :plain,
+  #    enable_starttls_auto: true
+  #}
+  #
+  #
 
   config.action_mailer.perform_caching = true
   # Ignore bad email addresses and do not raise email delivery errors.
